94a39445bfc389b4c60715b56646f0cea5963efe
plopez1985 <pl586546@gmail.com>
2021-10-28 22:24:29 +0000

    por fin

--- src/app.js
+++ src/app.js
@@ -9,3 +9,23 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var dominio = [".com", ".es", ".net", ".us", ".cl", ".io"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < dominio.length; l++) {
+        console.log(
+          pronoun[i]
+            .concat(adj[j])
+            .concat(noun[k])
+            .concat(dominio[l])
+        );
+      }
+    }
+  }
+}
